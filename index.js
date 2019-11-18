c608760e614b9632654720675aad7791cb5f24c9
Nuno Preguiça <preguica@users.noreply.github.com>
2019-11-18 10:26:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function MetricsByEndpoint(script, events) {
   console.log( "IN PLUGIN");
   console.log( script);
   console.log( script.plugins['metrics-by-endpoint']);
-  script.config.processor.myProcessEndpoint();
-  
+/*  script.config.processor.myProcessEndpoint();
+  */
   script.config.processor.metricsByEndpoint_beforeRequest = metricsByEndpoint_beforeRequest;
 script.config.processor.metricsByEndpoint_afterResponse = metricsByEndpoint_afterResponse;
 
